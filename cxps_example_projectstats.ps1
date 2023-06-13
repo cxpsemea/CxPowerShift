a5a88f0082fbe74cd0d9c403fe8c380b90f0f2d3
michael kubiaczyk <michael.kubiaczyk@checkmarx.com>
2023-06-13 08:16:20 +0200

    added token refresh, projectstats example caching

--- cxps_example_projectstats.ps1
+++ cxps_example_projectstats.ps1
@@ -12,10 +12,22 @@ $projectIDList = (get-content $projectsFile)
 $scan_limit = 10
 $cx1client = NewCx1Client $cx1url $iamurl $tenant $apikey "" #"http://localhost:8080"
 $outputFile = "Cx1 project scans history.csv"
+$outputfileExists = $false
 
-If (Test-Path -Path $outputFile) { Remove-Item -Path $outputFile -Force}
+$lastProjectScan = @{}
 
-
+If (Test-Path -Path $outputFile) {
+    $outputfileExists = $true
+    $existingData = (Import-Csv $outputFile -Delimiter ";")
+    
+    $lastPID = ""
+    $existingData | foreach-object {
+        if ( $_.ProjectID -ne $lastPID ) {
+            $lastPID = $_.ProjectID
+            $lastProjectScan[$lastPID] = $_.ScanID
+        }
+    }
+}
 
 $stages = @( "SourcePulling", "Queued", "ScanStart", "ScanEnd", "Finish" )
 
@@ -27,8 +39,8 @@ $regex = @{
     Finish = "Scan Completed"
 }
 
-function getTimestamps( $workflow ) {
-    $startTime = [datetime]$workflow[0].Timestamp
+function getTimestamps( $createdAt, $workflow ) {
+    $startTime = [datetime]$createdAt
     $zero = New-TimeSpan -Seconds 0
 
     $scaninfo = [ordered]@{
@@ -49,18 +61,18 @@ function getTimestamps( $workflow ) {
         FinishDelta = $zero
     }
 
-    $workflow | foreach-object {
-        $log = $_
+    foreach( $log in $workflow ) {
+        $log = $log
 
         $lastStage = "Start"
-        $stages | foreach-object {
-            $stage = $_
+        foreach( $stage in $stages ) {
             $delta = "$($stage)Delta"
             if ( $log.Info -match $regex[$stage] ) {
                 $stampTime = [datetime]$log.Timestamp
                 $timeDelta = $stampTime - $scaninfo[$lastStage]
                 $scaninfo[$stage] = $stampTime
                 $scaninfo[$delta] = $timeDelta
+                break
             }
             
             $lastStage = $stage
@@ -72,21 +84,27 @@ function getTimestamps( $workflow ) {
 
 function GetProjectScanHistory( $Cx1ProjectID ) {
     $offset = 0
-    $scan_count = $cx1client.GetScans( 1, $Cx1ProjectID ).filteredTotalCount
+    $scan_count = [int]($cx1client.GetScans( 1, $Cx1ProjectID ).filteredTotalCount)
 
     Write-Host "Project $Cx1ProjectID has $scan_count scans total"
 
     do {
         $scans = $cx1client.GetScans( $scan_limit, $Cx1ProjectID, "", "+created_at", $offset )
     
-        $scans.scans | foreach-object {
-            $workflow = $cx1client.GetScanWorkflow( $_.id )
-            $stamps = getTimestamps $workflow
+        foreach( $scan in $scans.scans) {
+            if ( $scan.id -eq $lastProjectScan[$Cx1ProjectID] ) {
+                Write-Host "Project $Cx1ProjectID scan $($scan.id) already in excel - skipping remaining scans"
+                return 
+            } else {
+                Write-Host "Processing project $Cx1ProjectID scan $($scan.id)"
+            }
+            $workflow = $cx1client.GetScanWorkflow( $scan.id )
+            $stamps = getTimestamps $scan.createdAt $workflow
     
             $stamps.ProjectID = $Cx1ProjectID
-            $stamps.ProjectName = $_.projectName
-            $stamps.ScanID = $_.id
-            $stamps.Status = $_.status
+            $stamps.ProjectName = $scan.projectName
+            $stamps.ScanID = $scan.id
+            $stamps.Status = $scan.status
             Add-Content -Path $outputFile -Value "$($stamps.ProjectID);$($stamps.ProjectName);$($stamps.ScanID);$($stamps.Status);$($stamps.Start);$($stamps.SourcePulling);$($stamps.SourcePullingDelta);$($stamps.Queued);$($stamps.QueuedDelta);$($stamps.ScanStart);$($stamps.ScanStartDelta);$($stamps.ScanEnd);$($stamps.ScanEndDelta);$($stamps.Finish);$($stamps.FinishDelta);"
         }
     
@@ -95,8 +113,9 @@ function GetProjectScanHistory( $Cx1ProjectID ) {
     } until ( $offset -gt $scan_count )
 }
 
-Add-Content -Path $outputFile -Value "sep=;"
-Add-Content -Path $outputFile -Value "ProjectID;ProjectName;ScanID;Status;Start;Source Pulling;SP Delta;Queued;Queue Delta;Scan Start;Start Delta;Scan End;End Delta;Finish;Finish Delta;"
+if ( -Not $outputfileExists ) {
+    Add-Content -Path $outputFile -Value "ProjectID;ProjectName;ScanID;Status;Start;Source Pulling;SP Delta;Queued;Queue Delta;Scan Start;Start Delta;Scan End;End Delta;Finish;Finish Delta;"
+}
 
 $projectIDList | foreach-object {
     GetProjectScanHistory $_
