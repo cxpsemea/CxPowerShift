978c3d754e37cf5b063a65310960083462844cfe
michael kubiaczyk <michael.kubiaczyk@checkmarx.com>
2023-06-28 12:27:57 +0200

    Updated projectstats example script

--- cxps_example_projectstats.ps1
+++ cxps_example_projectstats.ps1
@@ -3,22 +3,41 @@ param(
     $cx1url,
     $iamurl,
     $tenant,
-    $apikey
+    $apikey, 
+    $since = ""
 )
 
 Import-Module .\CxPowerShift
 
+if ( $since -ne "" ) {
+    $startTime = [datetime]::Parse( $since )
+} else {
+    $startTime = [datetime]::Parse( "2020-01-01 00:00:00")
+}
+Write-Host "Filtering for scans since $startTime"
+
 $projectIDList = (get-content $projectsFile)
-$scan_limit = 10
 $cx1client = NewCx1Client $cx1url $iamurl $tenant $apikey "" 
 $outputFile = "Cx1 project scans history.csv"
-$outputfileExists = $false
+$scan_limit = 10
+$writeHeaders = $true
+
+$stages = @( "Queued", "Running", "SourcePulling", "ScanQueued", "ScanStart", "ScanEnd" )
+
+$regex = @{
+    SourcePulling = "fetch-sources-.* started"
+    Running = "Scan running"
+    Queued = "reached, scan queued"
+    ScanQueued = "Queued in sast resource manager"
+    ScanStart = "sast-worker-.* started"
+    ScanEnd = "sast-worker-.* ended"
+}
+
 
 $lastProjectScan = @{}
 
 If (Test-Path -Path $outputFile) {
-    $outputfileExists = $true
-    $existingData = (Import-Csv $outputFile -Delimiter ";")
+    $existingData = Import-Csv $outputFile
     
     $lastPID = ""
     $existingData | foreach-object {
@@ -27,29 +46,29 @@ If (Test-Path -Path $outputFile) {
             $lastProjectScan[$lastPID] = $_.ScanID
         }
     }
-}
 
-$stages = @( "SourcePulling", "Queued", "ScanStart", "ScanEnd" )
-
-$regex = @{
-    SourcePulling = "fetch-sources-.* started"
-    Queued = "sast-rm-.* Queued in sast resource manager"
-    ScanStart = "sast-worker-.* started"
-    ScanEnd = "sast-worker-.* ended"
+    $writeHeaders = $false
 }
 
 function getTimestamps( $createdAt, $workflow ) {
     $startTime = [datetime]$createdAt
     $zero = New-TimeSpan -Seconds 0
 
-    $scaninfo = [ordered]@{
+    $scaninfo = [PSCustomObject]@{
         ProjectID = ""
         ProjectName = ""
         ScanID = ""
         Status = ""
+        FailReason = ""
+        LOC = 0
+        FileCount = 0
+        Incremental = $false
+        Preset = ""
         Start = $startTime
+        Queued = $zero #"Max concurent scans reached, scan queued (Position: 40)"
+        Running = $zero #"Scan running"
         SourcePulling = $zero #"fetch-sources-frankfurt started"
-        Queued = $zero #"sast-rm-frankfurt Queued in sast resource manager"
+        ScanQueued = $zero #"sast-rm-frankfurt Queued in sast resource manager"
         ScanStart = $zero #"sast-worker-frankfurt started"
         ScanEnd = $zero #"sast-worker-frankfurt ended"
         Finish = $zero #"Scan Completed"
@@ -61,23 +80,15 @@ function getTimestamps( $createdAt, $workflow ) {
         foreach( $stage in $stages ) {
             if ( $log.Info -match $regex[$stage] ) {
                 $stampTime = [datetime]$log.Timestamp
-                $scaninfo[$stage] = $stampTime
+                $scaninfo.$stage = $stampTime
                 break
             }
-            
-            $lastStage = $stage
         }        
     }
 
     return $scaninfo
 }
-
-# To do:
-#   + LOC
-#   + FileCount
-#   + Preset
-#   + Incremental
-function GetProjectScanHistory( $Cx1ProjectID ) {
+function GetProjectScanHistory( $Cx1ProjectID, $startTime ) {
     $offset = 0
     $scan_count = [int]($cx1client.GetScans( 1, $Cx1ProjectID ).filteredTotalCount)
 
@@ -87,14 +98,40 @@ function GetProjectScanHistory( $Cx1ProjectID ) {
         $scans = $cx1client.GetScans( $scan_limit, $Cx1ProjectID, "", "+created_at", $offset )
     
         foreach( $scan in $scans.scans) {
+            if ( [datetime]::Parse( $scan.createdAt ) -lt $startTime ) {
+                Write-Host "Project $Cx1ProjectID scan $($scan.id) created at $($scan.createdAt) - before cutoff $startTime "
+                return
+            }
             if ( $scan.id -eq $lastProjectScan[$Cx1ProjectID] ) {
                 Write-Host "Project $Cx1ProjectID scan $($scan.id) already in excel - skipping remaining scans"
             } else {
                 Write-Host "Processing project $Cx1ProjectID scan $($scan.id)"
             
                 $workflow = $cx1client.GetScanWorkflow( $scan.id )
-                $metadata = $cx1client.GetScanSASTMetadata( $scan.id )
                 $stamps = getTimestamps $scan.createdAt $workflow
+
+                try {
+                    $metadata = $cx1client.GetScanSASTMetadata( $scan.id )
+                    $stamps.LOC = $metadata.loc
+                    $stamps.FileCount = $metadata.fileCount
+                    $stamps.Incremental = $metadata.isIncremental
+                    $stamps.Preset = $metadata.queryPreset
+                } catch {
+                    Write-Warning "Failed to get metadata for scan $($scan.id): $_"
+                }
+                
+                if ( $scan.status -eq "Failed" ) {
+                    $stamps.FailReason = "zeebe" # default fail reason
+                    if ( $null -ne $scan.statusDetails ) {
+                        foreach ( $reason in $scan.statusDetails ) {
+                            if ( $reason.name -eq "sast" ) {
+                                if ( $reason.status -eq "failed" ) {
+                                    $scan.FailReason = $reason.details
+                                }
+                            }
+                        }
+                    }
+                }
         
                 $stamps.ProjectID = $Cx1ProjectID
                 $stamps.ProjectName = $scan.projectName
@@ -102,7 +139,14 @@ function GetProjectScanHistory( $Cx1ProjectID ) {
                 $stamps.Status = $scan.status
                 $stamps.Finish = $scan.updatedAt
 
-                Add-Content -Path $outputFile -Value "$($stamps.ProjectID);$($stamps.ProjectName);$($stamps.ScanID);$($stamps.Status);$($metadata.loc);$($metadata.fileCount);$($metadata.isIncremental);$($metadata.queryPreset);$($stamps.Start);$($stamps.SourcePulling);$($stamps.Queued);$($stamps.ScanStart);$($stamps.ScanEnd);$($stamps.Finish)"
+                if ($writeHeaders) {
+                    Export-Csv -Path $outputFile -InputObject $stamps -NoTypeInformation
+                    $writeHeaders = $false
+                } else {
+                    Export-Csv -Path $outputFile -InputObject $stamps -NoTypeInformation -Append
+                }
+
+                #Add-Content -Path $outputFile -Value "$($stamps.ProjectID);$($stamps.ProjectName);$($stamps.ScanID);$($stamps.Status);$failReason;$($metadata.loc);$($metadata.fileCount);$($metadata.isIncremental);$($metadata.queryPreset);$($stamps.Start);$($stamps.Queued);$($stamps.SourcePulling);$($stamps.ScanQueued);$($stamps.ScanStart);$($stamps.ScanEnd);$($stamps.Finish)"
             }
         }
     
@@ -111,14 +155,9 @@ function GetProjectScanHistory( $Cx1ProjectID ) {
     } until ( $offset -gt $scan_count )
 }
 
-if ( -Not $outputfileExists ) {
-    Add-Content -Path $outputFile -Value "ProjectID;ProjectName;ScanID;Status;LOC;Files;Incremental;Preset;Start;Source Pulling;Queued;Scan Start;Scan End;Finish"
-}
 
 $projectIDList | foreach-object {
-    GetProjectScanHistory $_
+    GetProjectScanHistory $_ $startTime
 }
-
-
 
 Remove-Module CxPowerShift
